a801ad8740b11abdedd71f758e2af534aae0d0e7
ZnoKunG <zno.ksy@gmail.com>
2022-10-21 21:07:41 +0700

    nestjs init

--- 10dayproject-api/src/server.ts
+++
@@ -1,9 +0,0 @@
-import router from "./routes/route";
-
-const express = require('express');
-const app = express();
-app.use(router);
-
-app.listen(3000, () => {
-    console.log("Listening to port 3000...")
-})
